16e8efa7efd6cb5caa47443b18476e5445e7e7f0
Yogesh Kumar <yogiky2002@gmail.com>
2020-08-23 18:59:36 +0530

    added data structure(array) to storing expenses and income

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // budget controller
 var budgetController = (function() {
 	
-	/*
-	var Expense = function(id , description , value) {
+		var Expense = function(id , description , value) {
 		this.id = id;
 		this.description = description;
 		this.value = value;
@@ -14,8 +13,60 @@ var budgetController = (function() {
 		this.value = value;
 	};
 	
-	*/
-	
+	var allExpenses = [];
+	var allIncomes = [];
+	var totalExpenses = 0;
+
+	var data = {
+		allItems: {
+			exp: [],
+			inc: []
+		},
+		totals: {
+			exp: 0,
+			inc: 0
+		}
+	};
+
+	return {
+		addItem: function(type, des, val) {
+
+			var newItem, ID;
+			
+			// [1,2,3,4,5], next ID = 6
+			// [1 2 5 7 9], next ID 10
+			// ID = last ID + 1
+
+			// create new ID
+			
+			if(data.allItems[type].length > 0) {
+				ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+			}
+			else {
+				ID =0;
+			}
+
+			//create newItem based on 'exp' or 'inc' type
+			if(type === 'exp') {
+				newItem = new Expense(ID, des, val);	
+			}
+			else if (type === 'inc') {
+				newItem = new Income(ID, des, val); 
+			}
+			
+			// Push it into our data structure
+			data.allItems[type].push(newItem);
+
+			// Return the new element
+			return newItem;
+		},
+
+		testing: function() {
+			console.log(data);
+		}
+
+	};
+
 })();
 
 // UI controller
@@ -61,10 +112,15 @@ var controller = (function(budgetCtrl , UICtrl) {
 	};
 
 	var ctrlAddItem = function() {
+		var input, newItem;
+
 		// 1. Get the filled input data
-		var input = UICtrl.getInput();
+		input = UICtrl.getInput();
 		
 		// 2. Add item to budgetController
+		newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+
+
 		// 3. Add item to UI
 		// 4. Calculate the budget
 		// 5. Display the budget on the UI
