d0170843109d2e55ffc9a4d1eb915a999190b4cc
Yogesh Kumar <yogiky2002@gmail.com>
2020-08-23 22:46:02 +0530

    added element to user interface

--- app.js
+++ app.js
@@ -76,7 +76,9 @@ var UIController = (function() {
 		inputType : '.add__type',
 		inputDescription : '.add__description',
 		inputValue : '.add__value',
-		inputBtn : '.add__btn'
+		inputBtn : '.add__btn',
+		incomeContainer : '.income__list',
+		expenseContainer : '.expenses__list'
 	};
 
 	return {
@@ -86,6 +88,30 @@ var UIController = (function() {
 				description : document.querySelector(DOMstrings.inputDescription).value,
 				value : document.querySelector(DOMstrings.inputValue).value
 			};			
+		},
+
+		addListItem: function(obj, type) {
+			
+			var html, newHtml, element;
+			// create html string with placeholder text
+
+			if(type === 'inc') {
+				element = DOMstrings.incomeContainer;
+				html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';			
+			}
+			else if (type === 'exp') {
+				element = DOMstrings.expenseContainer;
+				html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+
+			// replace the placeholder text with some actual data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%' , obj.description);
+			newHtml = newHtml.replace('%value%' , obj.value);
+			
+			// insert the HTML into the dom
+			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
 		},
 
 		getDOMstrings: function() {
@@ -122,6 +148,7 @@ var controller = (function(budgetCtrl , UICtrl) {
 
 
 		// 3. Add item to UI
+		UICtrl.addListItem(newItem, input.type);
 		// 4. Calculate the budget
 		// 5. Display the budget on the UI
 	};
